8d8162fdcd891696f53dfc1957816138badf8920
Dany <danywalls4@gmail.com>
2021-05-27 09:30:44 +0200

    pdf and bulma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularhtml2pdf';
+
+  downloadForm() {
+    const form = document.getElementById('form') || new HTMLElement();
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'whitesmoke',
+      scale: 3
+    };
+    html2canvas(form, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`form.pdf`);
+    });
+  }
 }
